d6d825c6f7b94d514d50a5b70699e132d3129372
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-06-11 17:36:44 +0100

    linked html elements to DOM

---
+++ calculator-scripts.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationsButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const outputTextElement = document.querySelector('[data-output]');
