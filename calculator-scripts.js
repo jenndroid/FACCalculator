05edd559e87d2942292bac4ea2c847ead2527ff9
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-06-16 14:37:12 +0100

    calc basic functions working still bugs and improvements to do

--- calculator-scripts.js
+++ calculator-scripts.js
@@ -1,6 +1,13 @@
+//I am going to leave this as-is, but if I want to have the js in a different file 
+//I would have to put this all in a class
+//It's not that complicated so I am going to stick this all in my html and unlink this file from head
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationsButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
-const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
-const outputTextElement = document.querySelector('[data-output]');
+const currentValueTextElement = document.querySelector('[data-current-value]');
+
+numberButtons.forEach(e => 
+    );
